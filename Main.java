d93b6e3d0c52f7ce65586d81356561872a33768c
Rahul-shah-123 <69796495+Rahul-shah-123@users.noreply.github.com>
2024-04-11 15:11:00 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+       System.out.println("This is my java program");
+       System.out.println("output form java program");
+       System.out.println(new Date());
+    }
+}
