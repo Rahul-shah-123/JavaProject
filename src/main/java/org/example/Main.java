1b2a980d2d3e4659d3e3fc456efeb82cb5ec8604
Rahul-shah-123 <69796495+Rahul-shah-123@users.noreply.github.com>
2024-09-02 22:13:06 +0530

    java code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+    /*  Understanding variable in Java */
+        int value1; // declaration
+        value1 = 10; // assignment
+        System.out.println(value1);
+
+        int value = 15,value2 ; // initialization
+        value2 = value;
+        System.out.println(value + value2);
+    }
+}
