6f9c8cfd20bb5b42ce80f65e96d97fb039002638
Rahul-shah-123 <69796495+Rahul-shah-123@users.noreply.github.com>
2024-09-03 22:31:34 +0530

    data type in the java

--- src/main/java/org/example/sample.java
+++ src/main/java/org/example/sample.java
@@ -1,11 +1,13 @@
 package org.example;
 public class sample {
     public static void main(String[] args) {
+
+       /* Data type Integer */
       int max = 2147483647 ; // 4 byte
       int min = -2147483648 ; // minimum value ;
-         short shortMax = 32767; //  2 byte
-        short shortMin = -32768; // minimum value of short
-                               long longMax = 9223372036854775807L; // 8 byte
+      short shortMax = 32767; //  2 byte
+      short shortMin = -32768; // minimum value of short
+      long longMax = 9223372036854775807L; // 8 byte
       long longMin = -9223372036854775808L; // minimum value of long
       byte byteMax = 127; // 1 byte
       byte byteMin = -128; //minimum value of byte
